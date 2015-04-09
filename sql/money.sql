adfbba621cc9305228b50d0c07f4ef69c2e07c8d
stevezheng <stevezheng1989@gmail.com>
2015-04-09 20:57:38 +0800

    update 变为充值表，增加充值平台

--- sql/money.sql
+++ sql/money.sql
@@ -1,7 +1,7 @@
 create table if not exists `yi_money` (
     `id` int(11) unsigned not null auto_increment,
     `moneyType` tinyint(1) not null default '0' comment '资金类型 0：金币， 1：押金， 2：流量',
-    `moneyUse` tinyint(1) not null default '0' comment '资金用途: 0：充值， 1：提现',
+    `moneyPlatform` varchar(10) not null default '' comment '充值途经',
     `moneyStatus` tinyint(1) not null default '0' comment '资金状态 0：待审核， 1：通过审核， -1： 拒绝审核',
     `moneyUserId` int(11) not null,
     `moneyValue` decimal(10, 2) not null default 0,
