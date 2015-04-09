c77734d0b62716a16b7ada19fe6c5ee6c7acc2fc
stevezheng <stevezheng1989@gmail.com>
2015-04-09 20:54:09 +0800

    fixed bankUserId的长度应该是11位

--- sql/bank.sql
+++ sql/bank.sql
@@ -2,7 +2,7 @@ create table if not exists `yi_bank` (
     `id` int(11) unsigned not null auto_increment,
     `bankType` tinyint(1) not null default '0' comment '银行类型 0：财付通， 1：支付宝， 2：银行卡',
     `bankStatus` tinyint(1) not null default '0' comment '银行状态 0：待审核， 1：通过审核， -1： 拒绝审核',
-    `bankUserId` int(1) not null,
+    `bankUserId` int(11) not null,
     `bankRealName` varchar(16) not null default '' comment '姓名/开户人姓名',
     `bankAccount` varchar(32) not null default '' comment '账号/卡号',
     `bankImage` varchar(512) not null default '' comment '账号信息截图',
