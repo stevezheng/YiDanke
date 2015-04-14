8a54b7666c2540059a3fe68b0abcc006272425a8
stevezheng <stevezheng1989@gmail.com>
2015-04-14 17:09:22 +0800

    add 任务发布第四步 上半部分

--- www/resource/js/angular/taobao.index.js
+++ www/resource/js/angular/taobao.index.js
@@ -1,18 +1,132 @@
 (function() {
+  //刷单费用
+  function cost(price) {
+    var coin = 0;
+    var base = 0.9;
+    if (0 < price && price <= 50) {
+      coin = 5.5;
+    } else if (50 < price && price <=  150) {
+      coin = 6.6;
+    } else if (150 < price && price <=  250) {
+      coin = 7.7;
+    } else if (250 < price && price <= 350) {
+      coin = 8.6;
+    } else if (350 < price && price <= 450) {
+      coin = 9.6;
+    } else if (450 < price && price <= 550) {
+      coin = 10.6;
+    } else if (550 < price && price <= 650) {
+      coin = 11.6;
+    } else if (650 < price && price <= 750) {
+      coin = 12.6;
+    } else if (750 < price && price <= 850) {
+      coin = 13.6;
+    } else if (850 < price && price <= 950) {
+      coin = 14.6;
+    } else if (950 < price && price <= 1050) {
+      coin = 15.6;
+    } else if (1050 < price && price <= 1150) {
+      coin = 16.8;
+    } else if (1150 < price && price <= 1250) {
+      coin = 17.8;
+    } else {
+      coin = 9999999;
+    }
+    coin += 0.5;
+    return (coin * base).toFixed(2);
+  }
+
   var TaobaoIndexModule = angular.module('YiApp.TaobaoIndex', ['angularFileUpload']);
 
   TaobaoIndexModule.controller('taobaoIndexCtrl', ['$scope', '$http', '$upload', function($scope, $http, $upload) {
-    $scope.step = 3; //默认值为:2
-
-    $scope.nextStep = function(step) {
-      $scope.step = step;
-    };
-
     $scope.upload = function(file) {
       return $upload.upload({
         url: '/home/index/upload',
         file: file
       })
+    };
+
+    $scope.step = 2; //默认值为:2
+
+     //任务花费明细
+    $scope.cost = {
+      totalCount: 0 //最终刷单数
+      , totalMoney: 0 //最终商品1、商品2、商品3的总价格
+
+      , fee: 0 //刷单一单的费用
+      , totalFee: 0 //刷单总的费用
+
+      , phone: 0 //移动端刷单附加费
+      , isPayback: true //是否平台返款
+      , payback: 0 //平台返款服务费
+      , speed: 0 //提升完成任务速度
+
+      , isExtendFee: false //是否平台加赏任务佣金
+      , extendFee: 2 //加赏单任务佣金
+      , totalExtendFee: 0 //加赏任务总佣金
+
+      , isInterval: false
+      , intervalTime: 10
+      , intervalCount: 1
+      , interval: 10
+
+      , selectPV: 60
+      , freePV: 0
+      , pv: 60
+
+      , cycleTime: 0
+      , totalCycle: 0
+    };
+
+    $scope.nextStep = function(step) {
+      //需要检查第二步里面的数据是否正确
+      if (step == 3) {
+        $scope.confirmItem();
+      }
+
+      //需要检查第三步里面的数据是否正确
+      if (step == 4) {
+        //判断刷单数是否是自定义的
+        var totalCount = 0;
+        if ($scope.item.totalCount == 0) {
+          totalCount = $scope.item.freeTotalCount;
+        } else {
+          totalCount = $scope.item.totalCount;
+        }
+        totalCount = parseInt(totalCount);
+        $scope.cost.totalCount = totalCount;
+
+        if (parseInt($scope.item.phoneCount) < parseInt($scope.item.pcCount)) {
+          alert('手机订单的数量至少要占据订单总数的50%');
+          return false;
+        }
+
+        if (parseInt($scope.item.phoneCount) + parseInt($scope.item.pcCount) != totalCount) {
+          alert('电脑端、手机端/Pad端订单总数需要为 '+totalCount+' 单');
+          return false;
+        }
+
+        //判断关键词总数
+        var keywordsCount = parseInt($scope.taobao.key1Count)
+          + parseInt($scope.taobao.key2Count)
+          + parseInt($scope.taobao.key3Count)
+          + parseInt($scope.taobao.key4Count)
+          + parseInt($scope.taobao.key5Count)
+          + parseInt($scope.tmall.key1Count)
+          + parseInt($scope.tmall.key2Count)
+          + parseInt($scope.tmall.key3Count)
+          + parseInt($scope.tmall.key4Count)
+          + parseInt($scope.tmall.key5Count);
+
+        if (keywordsCount != totalCount) {
+          alert('各关键词订单总数需要为 '+totalCount+' 单');
+          return false;
+        }
+
+        $scope.cost.payback = $scope.cost.totalCount * $scope.cost.totalMoney * 0.06;
+      }
+
+      $scope.step = step;
     };
 
     $scope.$watch('taobao.image', function () {
@@ -59,26 +173,34 @@
         })
     });
 
+    $scope.$watch('cost.extendFee', function() {
+      if ($scope.cost.extendFee < 2 || !angular.isNumber($scope.cost.extendFee)) {
+        $scope.cost.extendFee = 2;
+      }
+    });
+
+
     //step2 start
     $scope.extendCount = 1;
     $scope.itemFlag = false;
     $scope.transportFlag = false;
 
     $scope.item = {
-      name: ''
-      , url: ''
-      , tag1: ''
-      , tag2: ''
-      , money: null
-      , count: null
-      , searchMoney: null
-      , priceStart: null
-      , priceEnd: null
+      name: '' //商品名称
+      , url: '' //商品链接
+      , tag1: '' //规格1
+      , tag2: '' //规格2
+      , money: null //购买价格
+      , count: null //购买数量
+      , searchMoney: null //搜索价格
+      , priceStart: null //搜索价格开始
+      , priceEnd: null //搜索价格结束
       , position: '全国'
-      , totalCount: 3
-      , _totalCount: 3
-      , pcCount: 0
-      , phoneCount: 0
+      , totalCount: 3 //选择刷单数
+      , freeTotalCount: 3 //自定义刷单数
+      , pcCount: 0 //电脑端刷单数
+      , phoneCount: 0 //移动端刷单数
+      , tips: '' //下单提示
     };
 
     $scope.extendItem1 = {
@@ -109,26 +231,31 @@
       searchBox: true
       , keywordsCount: 1
       , key1: ''
+      , key1Count: 0
       , key1extend1: ''
       , key1extend2: ''
       , key1extend3: ''
       , key1extend4: ''
       , key2: ''
+      , key2Count: 0
       , key2extend1: ''
       , key2extend2: ''
       , key2extend3: ''
       , key2extend4: ''
       , key3: ''
+      , key3Count: 0
       , key3extend1: ''
       , key3extend2: ''
       , key3extend3: ''
       , key3extend4: ''
       , key4: ''
+      , key4Count: 0
       , key4extend1: ''
       , key4extend2: ''
       , key4extend3: ''
       , key4extend4: ''
       , key5: ''
+      , key5Count: 0
       , key5extend1: ''
       , key5extend2: ''
       , key5extend3: ''
@@ -139,26 +266,31 @@
       searchBox: false
       , keywordsCount: 1
       , key1: ''
+      , key1Count: 0
       , key1extend1: ''
       , key1extend2: ''
       , key1extend3: ''
       , key1extend4: ''
       , key2: ''
+      , key2Count: 0
       , key2extend1: ''
       , key2extend2: ''
       , key2extend3: ''
       , key2extend4: ''
       , key3: ''
+      , key3Count: 0
       , key3extend1: ''
       , key3extend2: ''
       , key3extend3: ''
       , key3extend4: ''
       , key4: ''
+      , key4Count: 0
       , key4extend1: ''
       , key4extend2: ''
       , key4extend3: ''
       , key4extend4: ''
       , key5: ''
+      , key5Count: 0
       , key5extend1: ''
       , key5extend2: ''
       , key5extend3: ''
@@ -236,35 +368,35 @@
         }
 
         if ($scope.taobao.keywordsCount > 0) {
-          if (!$scope.taobao.key1 && $scope.taobao.key1.length > 10) {
+          if (!$scope.taobao.key1 || $scope.taobao.key1.length > 10) {
             alert('淘宝商品关键字1，不能为空且输入中文不能超过10个字');
             return false;
           }
         }
 
         if ($scope.taobao.keywordsCount > 1) {
-          if (!$scope.taobao.key2 && $scope.taobao.key2.length > 10) {
+          if (!$scope.taobao.key2 || $scope.taobao.key2.length > 10) {
             alert('淘宝商品关键字2，不能为空且输入中文不能超过10个字');
             return false;
           }
         }
 
         if ($scope.taobao.keywordsCount > 2) {
-          if (!$scope.taobao.key3 && $scope.taobao.key3.length > 10) {
+          if (!$scope.taobao.key3 || $scope.taobao.key3.length > 10) {
             alert('淘宝商品关键字3，不能为空且输入中文不能超过10个字');
             return false;
           }
         }
 
         if ($scope.taobao.keywordsCount > 3) {
-          if (!$scope.taobao.key4 && $scope.taobao.key4.length > 10) {
+          if (!$scope.taobao.key4 || $scope.taobao.key4.length > 10) {
             alert('淘宝商品关键字4，不能为空且输入中文不能超过10个字');
             return false;
           }
         }
 
         if ($scope.taobao.keywordsCount > 4) {
-          if (!$scope.taobao.key5 && $scope.taobao.key5.length > 10) {
+          if (!$scope.taobao.key5 || $scope.taobao.key5.length > 10) {
             alert('淘宝商品关键字5，不能为空且输入中文不能超过10个字');
             return false;
           }
@@ -278,35 +410,35 @@
         }
 
         if ($scope.tmall.keywordsCount > 0) {
-          if (!$scope.tmall.key1 && $scope.tmall.key1.length > 10) {
+          if (!$scope.tmall.key1 || $scope.tmall.key1.length > 10) {
             alert('天猫商品关键字1，不能为空且输入中文不能超过10个字');
             return false;
           }
         }
 
         if ($scope.tmall.keywordsCount > 1) {
-          if (!$scope.tmall.key2 && $scope.tmall.key2.length > 10) {
+          if (!$scope.tmall.key2 || $scope.tmall.key2.length > 10) {
             alert('天猫商品关键字2，不能为空且输入中文不能超过10个字');
             return false;
           }
         }
 
         if ($scope.tmall.keywordsCount > 2) {
-          if (!$scope.tmall.key3 && $scope.tmall.key3.length > 10) {
+          if (!$scope.tmall.key3 || $scope.tmall.key3.length > 10) {
             alert('天猫商品关键字3，不能为空且输入中文不能超过10个字');
             return false;
           }
         }
 
         if ($scope.tmall.keywordsCount > 3) {
-          if (!$scope.tmall.key4 && $scope.tmall.key4.length > 10) {
+          if (!$scope.tmall.key4 || $scope.tmall.key4.length > 10) {
             alert('天猫商品关键字4，不能为空且输入中文不能超过10个字');
             return false;
           }
         }
 
         if ($scope.tmall.keywordsCount > 4) {
-          if (!$scope.tmall.key5 && $scope.tmall.key5.length > 10) {
+          if (!$scope.tmall.key5 || $scope.tmall.key5.length > 10) {
             alert('天猫商品关键字5，不能为空且输入中文不能超过10个字');
             return false;
           }
@@ -366,6 +498,20 @@
           }
         }
       }
+
+
+      var price = $scope.item.money * $scope.item.count;
+
+      if ($scope.extendItem1.money) {
+        price += $scope.extendItem1.money * $scope.extendItem1.count;
+      }
+      if ($scope.extendItem2.money) {
+        price += $scope.extendItem2.money * $scope.extendItem2.count;
+      }
+
+      $scope.cost.totalMoney = price;
+
+      $scope.cost.fee = cost(price);
 
       $scope.itemFlag = true;
     };
