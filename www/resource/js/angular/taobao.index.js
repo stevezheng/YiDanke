759381bfe30aa3951f18423463b020af3bf72e76
stevezheng <stevezheng1989@gmail.com>
2015-04-14 12:11:00 +0800

    add 发布商品第二步，确认提交信息

--- www/resource/js/angular/taobao.index.js
+++ www/resource/js/angular/taobao.index.js
@@ -33,6 +33,28 @@
         })
     });
 
+    $scope.$watch('extendItem1.image', function () {
+      if (!$scope.extendItem1.image) {
+        return false;
+      }
+      $scope
+        .upload($scope.extendItem1.image)
+        .success(function(data, status, headers, config) {
+          $scope.extendItem1.imagefile = data.filename;
+        })
+    });
+
+    $scope.$watch('extendItem2.image', function () {
+      if (!$scope.extendItem2.image) {
+        return false;
+      }
+      $scope
+        .upload($scope.extendItem2.image)
+        .success(function(data, status, headers, config) {
+          $scope.extendItem2.imagefile = data.filename;
+        })
+    });
+
     //step2 start
     $scope.extendCount = 1;
 
@@ -48,10 +70,12 @@
       , priceEnd: null
       , position: '全国'
     };
+
     $scope.extendItem1 = {
       name: ''
       , url: ''
       , image: ''
+      , imagefile: ''
       , tag1: ''
       , tag2: ''
       , money: null
@@ -63,6 +87,7 @@
       name: ''
       , url: ''
       , image: ''
+      , imagefile: ''
       , tag1: ''
       , tag2: ''
       , money: null
@@ -167,6 +192,171 @@
     };
 
     $scope.confirmItem = function() {
+      //检查商品信息
+      if (!$scope.item.name) {
+        alert('请检查商品名称是否正确');
+        return false;
+      }
+
+      if (!$scope.item.url) {
+        alert('请检查商品链接是否正确');
+        return false;
+      }
+
+      if (!$scope.item.money) {
+        alert('请检查单品售价是否正确');
+        return false;
+      }
+
+      if (!$scope.item.count) {
+        alert('请检查每单拍是否正确');
+        return false;
+      }
+
+      if ($scope.item.count * $scope.item.money <= 0) {
+        alert('请检查商品金额是否正确');
+        return false;
+      }
+
+      //检查淘宝搜索框
+      if ($scope.taobao.searchBox) {
+        if (!$scope.taobao.imagefile) {
+          alert('请检查淘宝商品主图是否正确');
+          return false;
+        }
+
+        if ($scope.taobao.keywordsCount > 0) {
+          if (!$scope.taobao.key1 && $scope.taobao.key1.length > 10) {
+            alert('淘宝商品关键字1，不能为空且输入中文不能超过10个字');
+            return false;
+          }
+        }
+
+        if ($scope.taobao.keywordsCount > 1) {
+          if (!$scope.taobao.key2 && $scope.taobao.key2.length > 10) {
+            alert('淘宝商品关键字2，不能为空且输入中文不能超过10个字');
+            return false;
+          }
+        }
+
+        if ($scope.taobao.keywordsCount > 2) {
+          if (!$scope.taobao.key3 && $scope.taobao.key3.length > 10) {
+            alert('淘宝商品关键字3，不能为空且输入中文不能超过10个字');
+            return false;
+          }
+        }
+
+        if ($scope.taobao.keywordsCount > 3) {
+          if (!$scope.taobao.key4 && $scope.taobao.key4.length > 10) {
+            alert('淘宝商品关键字4，不能为空且输入中文不能超过10个字');
+            return false;
+          }
+        }
+
+        if ($scope.taobao.keywordsCount > 4) {
+          if (!$scope.taobao.key5 && $scope.taobao.key5.length > 10) {
+            alert('淘宝商品关键字5，不能为空且输入中文不能超过10个字');
+            return false;
+          }
+        }
+      }
+      //检查天猫搜索框
+      if ($scope.tmall.searchBox) {
+
+        if (!$scope.tmall.imagefile) {
+          alert('请检查天猫商品主图是否正确');
+          return false;
+        }
+
+        if ($scope.tmall.keywordsCount > 0) {
+          if (!$scope.tmall.key1 && $scope.tmall.key1.length > 10) {
+            alert('天猫商品关键字1，不能为空且输入中文不能超过10个字');
+            return false;
+          }
+        }
+
+        if ($scope.tmall.keywordsCount > 1) {
+          if (!$scope.tmall.key2 && $scope.tmall.key2.length > 10) {
+            alert('天猫商品关键字2，不能为空且输入中文不能超过10个字');
+            return false;
+          }
+        }
+
+        if ($scope.tmall.keywordsCount > 2) {
+          if (!$scope.tmall.key3 && $scope.tmall.key3.length > 10) {
+            alert('天猫商品关键字3，不能为空且输入中文不能超过10个字');
+            return false;
+          }
+        }
+
+        if ($scope.tmall.keywordsCount > 3) {
+          if (!$scope.tmall.key4 && $scope.tmall.key4.length > 10) {
+            alert('天猫商品关键字4，不能为空且输入中文不能超过10个字');
+            return false;
+          }
+        }
+
+        if ($scope.tmall.keywordsCount > 4) {
+          if (!$scope.tmall.key5 && $scope.tmall.key5.length > 10) {
+            alert('天猫商品关键字5，不能为空且输入中文不能超过10个字');
+            return false;
+          }
+        }
+
+        if ($scope.extendCount > 1) {
+          if (!$scope.extendItem1.name) {
+            alert('请检查商品2,商品名是否正确');
+            return false;
+          }
+
+          if (!$scope.extendItem1.url) {
+            alert('请检查商品2,商品链接是否正确');
+            return false;
+          }
+
+          if (!$scope.extendItem1.money) {
+            alert('请检查商品2,商品售价是否正确');
+            return false;
+          }
+
+          if (!$scope.extendItem1.count) {
+            alert('请检查商品2,每单拍是否正确');
+            return false;
+          }
+
+          if ($scope.extendItem1.count * $scope.extendItem1.money <= 0) {
+            alert('请检查商品2,金额是否正确');
+            return false;
+          }
+        }
+
+        if ($scope.extendCount > 2) {
+          if (!$scope.extendItem2.name) {
+            alert('请检查商品3,商品名是否正确');
+            return false;
+          }
+
+          if (!$scope.extendItem2.url) {
+            alert('请检查商品3,商品链接是否正确');
+            return false;
+          }
+
+          if (!$scope.extendItem2.money) {
+            alert('请检查商品3,商品售价是否正确');
+            return false;
+          }
+
+          if (!$scope.extendItem2.count) {
+            alert('请检查商品3,每单拍是否正确');
+            return false;
+          }
+
+          if ($scope.extendItem2.count * $scope.extendItem2.money <= 0) {
+            alert('请检查商品3,金额是否正确');
+            return false;
+          }
+        }
+      }
       console.log($scope.item);
       console.log($scope.taobao);
       console.log($scope.tmall);
