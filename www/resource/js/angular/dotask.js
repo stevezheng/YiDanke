06113abd4f3ef195b49b33ab8276a736bad114ba
stevezheng <stevezheng1989@gmail.com>
2015-04-17 02:02:30 +0800

    add 做任务第四步

--- www/resource/js/angular/dotask.js
+++ www/resource/js/angular/dotask.js
@@ -13,27 +13,110 @@
     if (r != null) return unescape(r[2]); return null;
   }
 
-  var DoTaskModule = angular.module('YiApp.DoTask', []);
+  function checkUrl(val, url) {
+    if (val.slice(0, 7) != 'http://') {
+      alert(url + '请以http://开头');
+      return false;
+    }
 
-  DoTaskModule.controller('doTaskCtrl', function($scope, $http) {
-    $scope.step = 1;
+    if (val.slice(0, 18) != 'http://item.taobao'
+      && val.slice(0, 19) != 'http://detail.tmall') {
+      alert(url + '请输入正确的商品链接');
+      return false;
+    }
+
+    return true;
+  }
+
+  var DoTaskModule = angular.module('YiApp.DoTask', ['angularFileUpload']);
+
+  DoTaskModule.controller('doTaskCtrl', function($scope, $http, $upload) {
+    $scope.step = 3;
 
     $scope.checkUrlFlag = false;
     $scope.itemUrl = '';
 
+    $scope.item = {
+      url1: ''
+      , url2: ''
+      , url3: ''
+      , url4: ''
+    };
+
     $scope.nextStep = function(step) {
-      if (step == 3) {
+      if (step == 4) {
+        if (!$scope.item.url1) {
+          alert('请填写宝贝页地址1');
+          return false;
+        }
 
+        if (!$scope.item.url2) {
+          alert('请填写宝贝页地址2');
+          return false;
+        }
+
+        if (!$scope.item.url3) {
+          alert('请填写宝贝页地址3');
+          return false;
+        }
+
+        if (!$scope.item.url4) {
+          alert('请填写宝贝页地址4');
+          return false;
+        }
+
+        var url1 = $scope.item.url1;
+        var url2 = $scope.item.url2;
+        var url3 = $scope.item.url3;
+        var url4 = $scope.item.url4;
+
+        var r1 = checkUrl($scope.item.url1, '宝贝页地址1');
+        var r2 = checkUrl($scope.item.url2, '宝贝页地址2');
+        var r3 = checkUrl($scope.item.url3, '宝贝页地址3');
+        var r4 = checkUrl($scope.item.url4, '宝贝页地址4');
+
+        if (!r1 || !r2 || !r3 || !r4) {
+          return false;
+        }
+        if (url1 == url2 || url1 == url3 || url1 == url4
+          || url2 == url3 || url2 == url4 || url3 == url4) {
+          alert('请不要填写相同的链接');
+          return false;
+        }
+
+        if (!$scope.talk.imagefile) {
+          alert('请上传聊天记录截图');
+          return false;
+        }
       }
-
       $scope.step = step;
     };
+
+    $scope.talk = {};
+
+    $scope.upload = function(file) {
+      return $upload.upload({
+        url: '/home/index/upload',
+        file: file
+      })
+    };
+
+    $scope.$watch('talk.image', function () {
+      if (!$scope.talk.image) {
+        return false;
+      }
+      $scope
+        .upload($scope.talk.image)
+        .success(function(data, status, headers, config) {
+          $scope.talk.imagefile = data.filename;
+        })
+    });
 
     $scope.checkUrl = function(itemUrl) {
       var id = getQueryStringByUrl(itemUrl, 'id');
       var _id = getQueryStringByUrl($scope.doTask.taskUrl, 'id');
       if (id == _id) {
-        alert('链接地址正确');
+        alert('商品链接正确(请勿关闭此商品页面)');
         $scope.checkUrlFlag = true;
       } else {
         alert('链接地址错误');
@@ -56,7 +139,6 @@
         .success(function(res) {
           res.data.doTaskDeadline = moment(res.data.doTaskCreateTime).add(1, 'd').format('YYYY-MM-DD HH:mm');
           res.data.doTaskCountdown = moment(res.data.doTaskCreateTime).add(1, 'd').diff(moment(), 'hours');
-          console.log(res.data.doTaskCountdown);
           $scope.doTask = res.data;
         })
     }
