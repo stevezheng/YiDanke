c586c62db8ea05b4cda657459b9fe8e6d6259837
stevezheng <stevezheng1989@gmail.com>
2015-05-05 03:19:54 +0800

    新增 后台修改密码

--- App/Lib/Controller/Admin/SettingController.js
+++ App/Lib/Controller/Admin/SettingController.js
@@ -13,5 +13,38 @@ module.exports = Controller("Admin/BaseController", function(){
       if (self.isPost()) {
       }
     },
+
+    editPasswordAction: function() {
+      var self = this;
+      self.assign('title', '');
+
+      if (self.isGet()) {
+
+      }
+
+      if (self.isPost()) {
+        var oPassword = self.post('oPassword')
+          , password = self.post('password');
+
+        return D('admin')
+          .where({id: self.cAdmin.id, password: md5(oPassword)})
+          .find()
+          .then(function(res) {
+            if (!isEmpty(res)) {
+              return D('admin')
+                .where({id: self.cAdmin.id})
+                .update({password: md5(password)})
+                .then(function() {
+                  return self.success('修改密码成功');
+                })
+            } else {
+              return self.error(500, '旧密码不正确');
+            }
+          })
+          .then(function() {
+            return self.error(500, '修改密码失败');
+          })
+      }
+    },
   };
 });
